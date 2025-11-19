7df3e7f38e444ca6c3dd63335223b2f29f15669e
tommy gingras <tommy@studiowebux.com>
2025-11-18 22:42:32 -0500

    chore: removed deno codebase entirely

--- restcli-deno/scripts/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-deno task build:all
-chmod +x ../bin/restcli*
-
-mv ../bin/restcli* /usr/local/bin/
-
-echo "Installed !"
--- restcli-deno/scripts/uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -r ~/.restcli/
-rm /usr/local/bin/restcli
-rm /usr/local/bin/restcli-curl2http
-rm /usr/local/bin/restcli-run
